2895ba1d2c5c7cce0c008242435166656fb7bf95
kalpesh8910 <prajapatikalpesh8910@gmail.com>
2022-05-06 16:47:00 +0530

    commit

--- src/main/java/com/indexPage/SignUpIndexPage.java
+++ src/main/java/com/indexPage/SignUpIndexPage.java
@@ -18,8 +18,7 @@ public class SignUpIndexPage extends AbstractPage {
 	} 
 
 	@FindBy(xpath="//*[contains(text(),\"Register now!\")]")
-	WebElement Register_email;
-	
+	WebElement Register_email;	
 	public SignUpVerificationPage  ClickResgiterNow()
 	{
 		Common.clickOn(driver, Register_email);
@@ -28,8 +27,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(id = "login-email")
-	WebElement input_email;
-	
+	WebElement input_email;	
 	public SignUpVerificationPage enterEmail(String email) {
 		
 		Common.enterDataIn(driver, input_email, email);
@@ -37,8 +35,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//button[@type='submit']")
-	WebElement Get_started;
-	
+	WebElement Get_started;	
 	public SignUpVerificationPage ClickGetStarted() {
 		
 		Common.clickOn(driver, Get_started);
@@ -46,8 +43,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//*[@src=\"/static/media/uk.4dcc5075.jpg\"]")
-	WebElement Gbp_currency;
-	
+	WebElement Gbp_currency;	
 	public SignUpVerificationPage ClickGBPcurrency() {
 		
 		Common.clickOn(driver, Gbp_currency);
@@ -55,8 +51,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//*[contains(text(),\"Completely free! Register now without a credit card\")]")
-	WebElement select_free;
-	
+	WebElement select_free;	
 	public SignUpVerificationPage Clickfreeplan() {
 		
 		Common.clickOn(driver, select_free);
@@ -64,8 +59,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//*[contains(text(), \"Continue\")]")
-	WebElement click_continue;
-	
+	WebElement click_continue;	
 	public SignUpVerificationPage ClickContinue() {
 		
 		Common.clickOn(driver, click_continue);
@@ -73,8 +67,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//*[contains(text(), \"Continue\")]")
-	WebElement scroll_continue;
-	
+	WebElement scroll_continue;	
 	public SignUpVerificationPage ScrollContinue() {
 		
 		Common.scrollToElement(driver, scroll_continue);
@@ -82,8 +75,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(name = "name")
-	WebElement input_firstname;
-	
+	WebElement input_firstname;	
 	public SignUpVerificationPage Enterfirstname(String firstname) {
 		
 		Common.enterDataIn(driver, input_firstname, firstname);
@@ -91,8 +83,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(name = "companyName")
-	WebElement input_companyname;
-	
+	WebElement input_companyname;	
 	public SignUpVerificationPage Entercompanyname(String companyName) {
 		
 		Common.enterDataIn(driver, input_companyname, companyName);
@@ -100,8 +91,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(name = "password")
-	WebElement input_pwd;
-	
+	WebElement input_pwd;	
 	public SignUpVerificationPage EnterpWd(String password) {
 		
 		Common.enterDataIn(driver, input_pwd, password);
@@ -109,8 +99,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(name = "confirmPassword")
-	WebElement input_confirmpwd;
-	
+	WebElement input_confirmpwd;	
 	public SignUpVerificationPage EnterConfirmpWd(String Confirmpassword) {
 		
 		Common.enterDataIn(driver, input_confirmpwd, Confirmpassword);
@@ -118,8 +107,7 @@ public class SignUpIndexPage extends AbstractPage {
 	}
 	
 	@FindBy(xpath = "//*[contains(text(),\"Create Account\")]")
-	WebElement Click_CreateAccount;
-	
+	WebElement Click_CreateAccount;	
 	public SignUpVerificationPage createAccount() {
 		
 		Common.clickOn(driver, Click_CreateAccount);
