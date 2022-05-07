b9aa3a97423a9f7ca027fd02e9fc412e1d188c23
kalpesh8910 <prajapatikalpesh8910@gmail.com>
2022-05-07 10:45:22 +0530

    Commit with configured 7-5-2022

--- src/main/java/com/indexPage/SignUpIndexPage.java
+++ src/main/java/com/indexPage/SignUpIndexPage.java
@@ -3,10 +3,13 @@ package com.indexPage;
 /*Code changes
 
 We have know
- now we are done ad let me know once you are dread f
+We don't know how we can take it forward.
+and we have to do show.
+Now we are done ad let me know once you are dread f
+
 We are checking the 
 
-How we need to check nowenwfwfwfjebjfeifbjthat that we are working fine now.
+How we need to check that we are working fine now.
 
 now what is the problem out of it and we are defining the problem.
 */
